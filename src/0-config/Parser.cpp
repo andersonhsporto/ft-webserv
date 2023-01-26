8ea2adb4a8e5c618a9b4fb4be8a1739b066df5ed
Anderson Porto <anderson.higo2@gmail.com>
2023-01-26 19:43:13 -0300

    init commit

---
+++ src/0-config/Parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anderson on 26/01/23.
+//
+
+#include "Parser.hpp"
---
+++ src/0-config/Parser.hpp
@@ -0,0 +1,12 @@
+//
+// Created by anderson on 26/01/23.
+//
+
+#ifndef FT_WEBSERVER_SRC_0_CONFIG_PARSER_HPP
+#define FT_WEBSERVER_SRC_0_CONFIG_PARSER_HPP
+
+class Parser {
+
+};
+
+#endif //FT_WEBSERVER_SRC_0_CONFIG_PARSER_HPP
