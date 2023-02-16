e3fd878ff7f6b2649aaa5bb59844af0264f9f070
Davi <davimoreirafinanceiro@gmail.com>
2023-02-16 13:46:28 -0300

    criação de uma classe pool para monitoramento dos sockets criados.

---
+++ includes/0-config/Pool.hpp
@@ -0,0 +1,25 @@
+#ifndef POLL_HPP
+#define POLL_HPP
+
+#include "./../1-models/Socket.hpp"
+#include <vector>
+#include <poll.h>
+
+class Poll
+{
+	private:
+		size_t					_size;
+		std::vector<Socket *>	_sockets;
+		struct pollfd			*_poolfd_list;
+
+
+	public:
+		Poll(void);
+		~Poll();
+	
+		void		init(std::vector<Socket *> &sockets);
+		void		run(void);
+		Socket		*get_socket(size_t index);
+		short		get_event_return(size_t index);
+};
+#endif
